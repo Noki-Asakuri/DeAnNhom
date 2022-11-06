d4939908b4233c8645d4aed872745173cb55af43
Asakuri Noki <phungtanphat23@gmail.com>
2022-11-06 18:50:06 +0700

    Add config for auth.

--- DeAnNhom/App_Start/RouteConfig.cs
+++ DeAnNhom/App_Start/RouteConfig.cs
@@ -10,6 +10,18 @@ namespace DeAnNhom
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Product",
+                url: "Product/{action}/{name}",
+                defaults: new { controller = "Product", action = "Index", name = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Account",
+                url: "Account/{action}/{name}",
+                defaults: new { controller = "Account", action = "Index", name = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
